a7e878692b6b6e3fa7d42a97f0f53d60bd81de5e
rutujajamdar <68149595+rutujajamdar@users.noreply.github.com>
2023-04-24 22:46:52 +0530

    Add files via upload

---
+++ Assi12/Assignment12_2/app/custom-style.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomStyle]'
+})
+export class CustomStyleDirective {
+
+  constructor(public eobj : ElementRef) 
+  {
+    eobj.nativeElement.style.background = 'yellow';
+    eobj.nativeElement.style.fontWeight = 'bold';
+   }
+
+}
